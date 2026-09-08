c8eee8778414d7c239ccb4bb44e2303018cd414a
carlsondultra <carlsondultra@users.noreply.github.com>
2026-09-07 22:49:38 -0400

    created dashboard route

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,25 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <Card className="max-w-sm w-full">
+        <CardHeader>
+          <CardTitle>carls anatomy dashboard</CardTitle>
+          <CardDescription>
+            dashboard
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+            dashboard content
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
